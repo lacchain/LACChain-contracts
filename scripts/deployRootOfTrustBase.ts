05dacd80948fc7f15ef6d9856feec3a23328ca82
eum602 <eum602@gmail.com>
2023-04-12 09:43:27 -0500

    docs: add docs and scripts to deploy root of trust main contract

--- scripts/deployRootOfTrustBase.ts
+++ scripts/deployRootOfTrustBase.ts
@@ -1,15 +1,12 @@
 import { ethers, lacchain } from "hardhat";
-import { keccak256, toUtf8Bytes } from "ethers/lib/utils";
 
 async function main() {
   const accounts = lacchain.getSigners();
-  const artifactName = "RootOfTrust";
+  const artifactName = "RootOfTrustBase";
   const Artifact = await ethers.getContractFactory(artifactName, accounts[0]);
   console.log("Using Base Relay Address:", lacchain.baseRelayAddress);
   const revokeMode = 0; // only direct parent can revoke
   const isRootMaintainer = false; // means only contract owner maintains "depth" and "reovocation mode"
-  const didRegistryAddress = "0xce24fF4fC5339Ff18836c82B11Bcefd6840075Fc";
-  const delegateType = keccak256(toUtf8Bytes("DefaultDelegateType"));
   const instance = await lacchain.deployContract(
     Artifact,
     lacchain.baseRelayAddress,
@@ -17,14 +14,13 @@ async function main() {
     "did:web:lacchain.id:3DArjNYv1q235YgLb2F7HEQmtmNncxu7qdXVnXvPx22e3UsX2RgNhHyhvZEw1Gb5C", // root did
     "0xFFFCe4Cc7033746106986Aca1B8B8572B2f58B08", // root account manager
     revokeMode,
-    isRootMaintainer,
-    didRegistryAddress,
-    delegateType
+    isRootMaintainer
   );
   console.log(
     `${artifactName} instance successfully deployed at address: ` +
       instance.address
   );
+  // const contract = Artifact.attach(instance.address);
 }
 
 main().catch((error) => {
