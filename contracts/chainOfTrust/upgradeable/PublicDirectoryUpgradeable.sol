4c3cfb24c80b51a14d051fc8c395c26647a2a03d
eum602 <eum602@gmail.com>
2023-05-24 18:38:48 -0500

    feat: add 'Public Directory' Upgradeable Smart contract

--- contracts/chainOfTrust/upgradeable/PublicDirectoryUpgradeable.sol
+++ contracts/chainOfTrust/upgradeable/PublicDirectoryUpgradeable.sol
@@ -6,11 +6,14 @@ import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
 import "../../common/upgradeable/BaseRelayRecipientUpgradeable.sol";
 
+import "../IPublicDirectory.sol";
+
 contract PublicDirectoryUpgradeable is
     Initializable,
     OwnableUpgradeable,
     UUPSUpgradeable,
-    BaseRelayRecipientUpgradeable
+    BaseRelayRecipientUpgradeable,
+    IPublicDirectory
 {
     /// @custom:oz-upgrades-unsafe-allow constructor
     constructor() {
@@ -18,9 +21,9 @@ contract PublicDirectoryUpgradeable is
     }
 
     function initialize(address trustedForwarderAddress) public initializer {
+        __BaseRelayRecipient_init(trustedForwarderAddress);
         __Ownable_init();
         __UUPSUpgradeable_init();
-        __BaseRelayRecipient_init(trustedForwarderAddress);
     }
 
     function _authorizeUpgrade(
@@ -47,5 +50,198 @@ contract PublicDirectoryUpgradeable is
         require(success, "SCF");
 
         return abi.decode(bytesSender, (address));
+    }
+
+    /***************** CORE METHODS **********************/
+    mapping(uint256 => member) public memberDetails;
+    mapping(address => uint256) public id;
+    uint256 public memberCounter;
+    uint256 prevBlock;
+    mapping(uint256 => mapping(address => bool)) public isCot;
+
+    function addMember(setMember memory _member) external onlyOwner {
+        memberCounter++;
+        uint256 memberId = memberCounter;
+        _associateDid(_member.did, memberId);
+
+        uint256 currentTimestamp = block.timestamp;
+        member storage m = memberDetails[memberId];
+        m.name = _member.name;
+        m.iat = currentTimestamp;
+
+        if (_member.expires) {
+            require(_member.exp > block.timestamp, "IET");
+            m.exp = _member.exp;
+            m.expires = true;
+        }
+
+        if (_member.chainOfTrustAddress != address(0)) {
+            _addCoTAddress(_member.chainOfTrustAddress, memberId);
+        }
+        emit MemberChanged(
+            memberId,
+            _member.did,
+            currentTimestamp,
+            m.exp,
+            _member.expires,
+            currentTimestamp,
+            prevBlock
+        );
+        prevBlock = block.number;
+    }
+
+    function _validateAddressAndMemberCounter(
+        address cotAddress,
+        uint256 memberId
+    ) private view {
+        require(cotAddress != address(0), "IA");
+        require(memberCounter >= memberId, "MIdDE");
+    }
+
+    function associateCoTAddressByDid(
+        address cotAddress,
+        string memory did
+    ) external onlyOwner {
+        address didAddr = _computeAddress(did);
+        uint256 memberId = id[didAddr];
+        _validateAddressAndMemberCounter(cotAddress, memberId);
+        _addCoTAddress(cotAddress, memberId);
+        prevBlock = block.number;
+    }
+
+    function _addCoTAddress(address cotAddress, uint256 memberId) private {
+        require(!isCot[memberId][cotAddress], "CAA");
+        isCot[memberId][cotAddress] = true;
+        emit CoTChange(cotAddress, memberId, true, prevBlock);
+    }
+
+    function disassociateCoTAddressByDid(
+        address cotAddress,
+        string memory did
+    ) external onlyOwner {
+        address didAddr = _computeAddress(did);
+        uint256 memberId = id[didAddr];
+        _validateAddressAndMemberCounter(cotAddress, memberId);
+        require(isCot[memberId][cotAddress], "CNATM");
+        isCot[memberId][cotAddress] = false;
+        emit CoTChange(cotAddress, memberId, false, prevBlock);
+        prevBlock = block.number;
+    }
+
+    // many dids may map to the same member description
+    function associateDid(
+        string memory did,
+        string memory didToAssociate
+    ) external onlyOwner {
+        address didAddr = _computeAddress(did);
+        uint256 memberId = id[didAddr];
+        _validateMemberIdExists(memberId);
+        _associateDid(didToAssociate, memberId);
+        prevBlock = block.number;
+    }
+
+    function _associateDid(string memory did, uint256 memberId) private {
+        address didAddr = _computeAddress(did);
+        require(id[didAddr] == 0, "DAE");
+        id[didAddr] = memberId;
+        emit DidAssociated(did, memberId, prevBlock);
+    }
+
+    function disassociateDid(
+        string memory did,
+        string memory didToDisassociate
+    ) external onlyOwner {
+        address didAddr1 = _computeAddress(did);
+        uint256 memberId1 = id[didAddr1];
+        address didAddr = _computeAddress(didToDisassociate);
+        uint256 memberId = id[didAddr];
+        require(memberId1 == memberId, "ALODRPE");
+        _validateMemberIdExists(memberId);
+        id[didAddr] = 0; // freed up
+        emit DidDisassociated(didToDisassociate, memberId, prevBlock);
+        prevBlock = block.number;
+    }
+
+    function removeMemberByDid(string memory did) external onlyOwner {
+        address didAddr = _computeAddress(did);
+        uint256 memberId = id[didAddr];
+        _validateMemberIdExists(memberId);
+
+        member storage memberDetail = memberDetails[memberId];
+        uint256 currentTimestamp = block.timestamp;
+        memberDetail.exp = currentTimestamp;
+        if (!memberDetail.expires) {
+            memberDetail.expires = true;
+        }
+        emit MemberChanged(
+            memberId,
+            did,
+            memberDetail.iat,
+            currentTimestamp,
+            true,
+            currentTimestamp,
+            prevBlock
+        );
+        prevBlock = block.number;
+    }
+
+    function updateMemberDetailsByDid(
+        setMember memory _member
+    ) external onlyOwner {
+        address didAddr = _computeAddress(_member.did);
+        uint256 memberId = id[didAddr];
+        _validateMemberIdExists(memberId);
+
+        uint256 currentTimestamp = block.timestamp;
+        member storage m = memberDetails[memberId];
+        if (bytes(_member.name).length > 0) {
+            m.name = _member.name;
+        }
+        m.uat = currentTimestamp;
+        if (_member.expires) {
+            require(_member.exp > block.timestamp, "IET");
+            m.exp = _member.exp;
+            if (!m.expires) {
+                m.expires = true;
+            }
+        } else {
+            if (m.expires) {
+                m.expires = false;
+                m.exp = 0;
+            }
+        }
+        emit MemberChanged(
+            memberId,
+            _member.did,
+            m.iat,
+            m.exp,
+            _member.expires,
+            currentTimestamp,
+            prevBlock
+        );
+        prevBlock = block.number;
+    }
+
+    function getMemberDetails(
+        string memory did
+    ) public view returns (fullDetails memory foundMember) {
+        address didAddr = _computeAddress(did);
+        uint256 memberId = id[didAddr];
+        member memory m = memberDetails[memberId];
+        foundMember.memberData = m;
+        foundMember.memberId = memberId;
+    }
+
+    function _computeAddress(
+        string memory txt
+    ) private pure returns (address addr) {
+        bytes32 h = keccak256(abi.encode((txt)));
+        assembly {
+            addr := h
+        }
+    }
+
+    function _validateMemberIdExists(uint256 memberId) private pure {
+        require(memberId > 0, "DNR");
     }
 }
--- docs/tech/configuration.md
+++ docs/tech/configuration.md
@@ -10,8 +10,10 @@ of how that file would look like for the lacchain network your are willing to co
 
 ```ts
 import { HardhatUserConfig, extendEnvironment } from "hardhat/config";
+import "@openzeppelin/hardhat-upgrades";
 import "@nomicfoundation/hardhat-toolbox";
 import "./extender";
+import "hardhat-contract-sizer";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.18",
@@ -27,12 +29,24 @@ const config: HardhatUserConfig = {
         "0x8f2a55949038a9610f50fb23b5883af3b4ecb3c3bb792cbcefbd1542c692be63",
         "0xc87509a1c067bbde78beb793e6fa76530b6382a4c0241e5e4a9ec0a0f44dc0d3",
         "0xae6ae8e5ccbfb04590405997ee2d52d2b330726137b875053c36d94e974d162f",
+        "0x5806be79fadd1ffea518d2183322c0b8d7469a521f7523a8dba4fff5a5b48f4e",
+        "0x775473c7e3f62cca7f2e2832167d267ffe28f716b40e369e985c98e0e38ebc6d",
       ],
+      chainId: 648540,
     },
   },
   gasReporter: {
     enabled: true,
     currency: "USD",
+  },
+  contractSizer: {
+    alphaSort: true,
+    disambiguatePaths: false,
+    runOnCompile: true,
+    strict: true,
+  },
+  mocha: {
+    timeout: 500000,
   },
 };
 
@@ -40,6 +54,7 @@ const config: HardhatUserConfig = {
 // lacchain Mainnet: 0xEAA5420AF59305c5ecacCB38fcDe70198001d147
 // lacchain Open-protesnet: 0xa4B5eE2906090ce2cDbf5dfff944db26f397037D
 extendEnvironment((hre) => {
+  console.log("calling from hardhat config");
   hre.lacchain.baseRelayAddress = "0xa4B5eE2906090ce2cDbf5dfff944db26f397037D";
 });
 
